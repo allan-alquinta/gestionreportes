f308e0eb28c66d5e2cc8f4a10497859d365105e0
allan-alquinta <juniorsaid2002@gmail.com>
2025-11-12 16:56:42 -0300

    Login
    Codigos escritos relacionados al login

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value.trim();
+
+  if (email === "admin@biokelp.com" && password === "12345") {
+    alert("Inicio de sesión exitoso");
+    // Aquí podrías redirigir al dashboard:
+    window.location.href = "dashboard.html";
+  } else {
+    alert("Credenciales inválidas");
+  }
+});
